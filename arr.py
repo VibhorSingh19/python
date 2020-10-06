354be10f6f271d02210a0911397b7c065214bc3c
Vibhor Singh <vibhor.formal@gmail.com>
2020-10-06 11:47:35 +0530

    updated

--- arr.py
+++ arr.py
@@ -1,4 +1,4 @@
-
+#first
 print("Enter 5 numbers...")
 import array 
    
@@ -7,4 +7,9 @@ arr = array.array('i', [])
 for i in range(0,5):
 	num1=int(input())
 	arr.append(num1)
+print(arr)
+
+#secound
+num2=int(input("Enter a index val."))
+arr.pop(num2)
 print(arr)
