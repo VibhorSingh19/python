1c39585096b5a0b45d621294ae673b4dc5fd032d
Vibhor Singh <vibhor.formal@gmail.com>
2020-10-06 11:51:25 +0530

    updated

--- arr.py
+++ arr.py
@@ -13,3 +13,13 @@ print(arr)
 num2=int(input("Enter a index val."))
 arr.pop(num2)
 print(arr)
+
+#third
+num3=int(input("Enter a number to be searched.."))
+for i in arr:
+	if(i==num3):
+		if(num3%2==0):
+			print("Number is present,It is Even")
+		else:
+		    print("Number is present,It is Odd")	
+	
