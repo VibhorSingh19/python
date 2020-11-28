52ed78b40c7f60ec6d68744e1b02a0e81e17dc43
Vibhor Singh <vibhor.formal@gmail.com>
2020-11-28 22:45:18 +0530

    gui

--- calculator.py
+++ calculator.py
@@ -1,41 +1,14 @@
-# Python program to create a simple GUI 
-# calculator using Tkinter 
-
-# import everything from tkinter module 
 from tkinter import *
-
-# globally declare the expression variable 
 expression = "" 
-
-
-# Function to update expressiom 
-# in the text entry box 
 def press(num): 
-	# point out the global expression variable 
 	global expression 
-
-	# concatenation of string 
 	expression = expression + str(num) 
-
-	# update the expression by using set method 
 	equation.set(expression) 
-
-
-# Function to evaluate the final expression 
 def equalpress(): 
-	# Try and except statement is used 
-	# for handling the errors like zero 
-	# division error etc. 
-
-	# Put that code inside the try block 
-	# which may generate the error 
 	try: 
 
 		global expression 
 
-		# eval function evaluate the expression 
-		# and str function convert the result 
-		# into string 
 		total = str(eval(expression)) 
 
 		equation.set(total) 
